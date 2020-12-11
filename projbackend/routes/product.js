e38f8459cb46d2f697137f92815226fd15df3543
unknown <imhitesh007@gmail.com>
2020-12-11 23:12:54 +0530

    PRODUCT BY ID

---
+++ projbackend/routes/product.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const {} = require("../controllers/product")
+const {} = require("../controllers/auth")
+const {} = require("../controllers/user")
+
+
+module.exports = router;
