bc63da6a0ef400e62b30b35b89ee6deb68b51999
unknown <imhitesh007@gmail.com>
2020-12-12 01:38:25 +0530

    Get product by id middleware added

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {} = require("../controllers/product")
-const {} = require("../controllers/auth")
-const {} = require("../controllers/user")
+const { getProductById } = require("../controllers/product")
+const { isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
+const { getUserById } = require("../controllers/user")
 
+//all of params
+router.param("userId", getUserById);
+router.param("productId", getProductById);
+
+//all of actual routes
 
 module.exports = router;
