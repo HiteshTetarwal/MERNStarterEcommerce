ad02a85f70cca29283aef56392462cb6aac27147
unknown <imhitesh007@gmail.com>
2020-12-13 01:18:32 +0530

    alignment changed

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -10,6 +10,10 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 //all of actual routes
-router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
+router.post("/product/create/:userId", 
+    isSignedIn, 
+    isAuthenticated, 
+    isAdmin, 
+    createProduct);
 
 module.exports = router;
