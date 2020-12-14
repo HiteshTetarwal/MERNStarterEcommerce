3de402f33cc594a33e9baec1e51dd4290a64f5d8
HiteshTetarwal <imhitesh007@gmail.com>
2020-12-15 00:09:36 +0530

    photo middle ware added

--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, createProduct } = require("../controllers/product")
+const { getProductById, createProduct, getProduct, photo } = require("../controllers/product")
 const { isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const { getUserById } = require("../controllers/user")
 
@@ -16,6 +16,8 @@ router.post("/product/create/:userId",
     isAdmin, 
     createProduct);
 
-router.get();
+router.get("/product/:productId", getProduct);
+
+router.get("/product/photo/:productId", photo)
 
 module.exports = router;
