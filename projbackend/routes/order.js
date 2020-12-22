ccec1528358d03703da4a0c22dfc85a5eed1cea8
HiteshTetarwal <imhitesh007@gmail.com>
2020-12-23 00:37:28 +0530

    Update the order status

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -5,7 +5,7 @@ const { isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user")
 const { updateStock } = require("../controllers/product")
 
-const { getOrderById, createOrder, getAllOrders } = require("../controllers/order")
+const { getOrderById, createOrder, getAllOrders, getOrderStatus, updateStatus } = require("../controllers/order")
 
 //params
 router.param("userId", getUserById)
@@ -24,6 +24,27 @@ router.post(
 );
 
 //read
-router.get("/order/all/:userId", isSignedIn, isAuthenticated, isAdmin, getAllOrders)
+router.get("/order/all/:userId", 
+    isSignedIn, 
+    isAuthenticated, 
+    isAdmin, 
+    getAllOrders
+);
+
+//status of order
+router.get("/order/status/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getOrderStatus
+);
+
+router.put("/order/:orderId/status/:userId", 
+    isSignedIn, 
+    isAuthenticated, 
+    isAdmin, 
+    updateStatus
+);
+
 
 module.exports = router;
