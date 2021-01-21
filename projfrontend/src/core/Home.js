e7a9206cc90b3262ca3d5270613d960e9cbb4972
HiteshTetarwal <imhitesh007@gmail.com>
2021-01-22 00:25:08 +0530

    BACKEND FRONTEND CONNECTION

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { API } from '../backend';
 import "../styles.css";
 
 export default function Home() {
+    console.log("API is", API);
     return (
         <div>
-            <h1 className="text-white">Hello front end</h1>
+            <h1 className="text-white">Hello front end {API}</h1>
         </div>
     )
 }
