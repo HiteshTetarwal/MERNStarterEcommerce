2646d331412506d2a4640379745b4ec26b83cf3b
HiteshTetarwal <imhitesh007@gmail.com>
2021-01-24 20:30:24 +0530

    bug fix with history pathname

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,38 +1,51 @@
 import React from 'react';
 import {Link, withRouter} from 'react-router-dom';
 
-const Menu = () => {
+const currentTab = (history, path) => {
+    if (history.location.pathname === path){
+        return {color: "#2ecc72"};
+    } else {
+        return {color: "#FFFFFF"};
+    }
+}
+
+const Menu = ({history}) => {
     return (
         <div>
         <ul className="nav nav-tabs bg-dark">
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/")} className="nav-link" to="/">
                 Home
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/cart")} className="nav-link" to="/cart">
                 Cart
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/user/dashboard")} className="nav-link" to="/user/dashboard">
                 Dashboard
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "/admin/dashboard")} className="nav-link" to="/admin/dashboard">
                 A. Dashboard
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link style={currentTab(history, "signup")} className="nav-link" to="/signup">
+                SignUp
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link style={currentTab(history, "signin")} className="nav-link" to="/signin">
                 SignIn
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/">
-                SignUp
+                <Link style={currentTab(history, "/signout")} className="nav-link" to="/signout">
+                SignOut
                 </Link>
             </li>
         </ul>
