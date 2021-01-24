780dcaf6e3d22f287e7ccf073170b0669cf544b3
HiteshTetarwal <imhitesh007@gmail.com>
2021-01-24 20:36:34 +0530

    sign up works

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './core/Home';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from './user/Signup';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/signup" exact component={Signup} />
             </Switch>
         </BrowserRouter>
     )
