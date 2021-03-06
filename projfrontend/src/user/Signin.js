2c8e8b8f03348c2c87373bcc9ef065a901c4d506
HiteshTetarwal <imhitesh007@gmail.com>
2021-03-06 18:11:36 +0530

    SignOut show on sign in

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -7,8 +7,8 @@ import { signin, authenticate, isAuthenticated } from "../auth/helper";
 const Signin = () => {
 
     const [values, setValues] = useState({
-        email : "",
-        password : "",
+        email : "d@hitesh.com",
+        password : "12345",
         error : "",
         loading : false,
         didRedirect : false
